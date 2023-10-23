5baf93e1c94378e783bd087b141b7757c9870a1f
liuxing2 <liuxing2@genomics.cn>
2023-10-23 14:08:39 +0800

    gpu parameter debug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("PYPI.rst", "r") as f:
 
 setup(
     name='stereosite',
-    version='1.0.6',
+    version='1.0.7',
     author='LiuXing',
     author_email='liuxing2@genomics.cn',
     description=('Analysis spatial transcriptomics data'),
--- stereosite/cn/deconvolution.py
+++ stereosite/cn/deconvolution.py
@@ -55,16 +55,12 @@ class Cell2location():
         self.adata_vis_file = adata_vis_file
         self.results_folder = out_dir
         self.N_cells_per_location = int((bin_size*500/10000)**2) if bin_size > 1 else 1 #calculate the cell number per bin based on bin size
-        if gpu.isinstance(bool):
-            self.gpu = gpu
-        elif gpu.upper() == 'TRUE':
+        if gpu.upper() == 'TRUE':
             self.gpu = True
         elif gpu.upper() == 'FALSE':
             self.gpu = False
-        elif gpu.isinstance(int):
-            self.gpu = f"cuda:{gpu}"
         else:
-            raise Exception("parameter gpu must be bool or int!!!")
+            self.gpu = f"cuda:{gpu}"
         self.ref_run_name = f'{self.results_folder}/reference_signatures'
         self.run_name = f'{self.results_folder}/cell2location_map'
         self.figures = f'{self.results_folder}/figures'
